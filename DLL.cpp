8999938398ec17a338e46c588b6cd4979fd95b8f
bryan <bryan@macbook-de-bryan.home>
2020-10-08 20:52:15 +0200

    Done with constructor and push method

--- DLL.cpp
+++ DLL.cpp
@@ -18,5 +18,51 @@ using namespace std;
 		numSongs=1;
 	}
 
+	void DLL::push(string t, string a, int m, int s){
+		if (numSongs == 0){
+			first = new DNode(t,a,m,s);
+			last = first;
+			numSongs++;
+			//cout<<"added first"
+		}
+		else{
+			DNode *n = new DNode(t,a,m,s);
+			n->next = NULL;
+			n->prev = last;
+			last = n;
+			//cout<<"pushed at the end"
+		}
+
+	}
+
+	Song* DLL::pop(){
+	}//does what you'd think
+
+	int DLL::remove(string t){
+	}
+	void DLL::makeRandom(){
+	// randomizes the order of the list
+	}
+	void DLL::moveUp(string t){
+		// moves song with title s up one in the playlist.
+	}
+			                        //If it is at the beginning of the list,
+			                        //it will be moved to the end of the list.
+
+	void DLL::moveDown(string t){
+		//moves song with title s down one in the playlist.
+	}
+									//If it is at the end of the list, it will move to
+									//beginning of the list.
+	void DLL::listDuration(int *tm, int *ts){
+		// gets the total list duration in minutes (passed in as pointers)
+	}
+												// and seconds (again, passed in as pointers)
+	void DLL::printList(){
+
+	}
+	DLL::~DLL(){
+	}
+
 
 
