6d4c61ae1a82d13c2494fa73501ac8644ef80136
bryan <bryan@macbook-de-bryan.home>
2020-10-11 17:47:10 +0200

    remove, moveUp and moveDown perfectly working

--- DLL.cpp
+++ DLL.cpp
@@ -30,6 +30,7 @@ using namespace std;
 			DNode *n = new DNode(t,a,m,s);
 			n->next = NULL;
 		    last->next = n;
+		    n->prev = last;
 			last = n;
 			numSongs+=1;
 			//cout<<"pushed at the end"
@@ -39,14 +40,12 @@ using namespace std;
 	Song* DLL::pop(){
 		DNode *temp = last;
 		Song *x = temp->song;
-		if (numSongs==1){
+		if (temp->prev==NULL){
 			first = NULL;
 			last = NULL;
 		}
-		else if (numSongs>1){
-			last = last->prev;
-			delete temp;
-			last->next = NULL;
+		else{
+			temp->prev->next = NULL;
 			numSongs--;
 		}
 		return x;
@@ -54,38 +53,113 @@ using namespace std;
 
 	int DLL::remove(string t){
 		int count = 0;
-		DNode *temp = first;
-		while (temp->song->title!=t){
-			temp = temp->next;
-			count++;
+		DNode *temp;
+		for (temp=first; temp!=NULL; temp=temp->next, count++){
+			if (temp->song->title==t){
+				if (temp->prev==NULL){
+					//cout << "first song" << endl;
+					first = temp->next;
+				}
+				else if (temp->next==NULL){
+					//cout << "pop called"<<endl;
+				    pop();
+				}
+				else{
+					//cout << "middle song"<<endl;
+					temp->prev->next = temp->next;
+					temp->next->prev = temp->prev;
+				}
+				cout << "Removing: "<< temp->song->title << ", " << temp->song->artist<<"............................."<<temp->song->min<<":"<<temp->song->sec;
+				cout << endl;
+				delete temp;
+			}
 		}
-		if (count == numSongs){
-			pop();
-			return 0; //didn't know what to return an int for so I used it to distinguish btw pop() and remove()
-		}
-		else{
-			cout << "Removing: "<< temp->song->title << ", " << temp->song->artist<<"............................."<<temp->song->min<<":"<<temp->song->sec;
-			cout << endl;
-			temp->prev->next = temp->next;
-			temp->next->prev = temp->prev;
-			delete temp;
-			return 1;
-		}
+			return count;
 	}
+
 	void DLL::makeRandom(){
 	// randomizes the order of the list
 	}
 	void DLL::moveUp(string t){
-		// moves song with title s up one in the playlist.
+		DNode *temp;
+		int count = 0;//makes sure the target title isn't recognized twice within the for loop
+		for (temp=first; temp!=NULL; temp=temp->next){
+			if (temp->song->title==t){
+				if (temp->prev==NULL){
+					cout << "first song"<<endl;
+					string lastTitle = last->song->title;
+					string lastArtist = last->song->artist;
+					int lastMin = last->song->min;
+					int lastSec = last->song->sec;
+					last->song->title = temp->song->title;
+					last->song->artist = temp->song->artist;
+					last->song->min = temp->song->min;
+					last->song->sec = temp->song->sec;
+					temp->song->title = lastTitle;
+					temp->song->artist = lastArtist;
+					temp->song->min = lastMin;
+					temp->song->sec = lastSec;
+					count++;
+
+				}
+				else if (count==0){
+					cout << "middle song"<<endl;
+					string prevTitle = temp->prev->song->title;
+					string prevArtist = temp->prev->song->artist;
+					int prevMin = temp->prev->song->min;
+					int prevSec = temp->prev->song->sec;
+					temp->prev->song->title = temp->song->title;
+					temp->prev->song->artist = temp->song->artist;
+					temp->prev->song->min = temp->song->min;
+					temp->prev->song->sec = temp->song->sec;
+					temp->song->title = prevTitle;
+					temp->song->artist = prevArtist;
+					temp->song->min = prevMin;
+					temp->song->sec = prevSec;
+				}
+			}
+		}
 	}
-			                        //If it is at the beginning of the list,
-			                        //it will be moved to the end of the list.
 
 	void DLL::moveDown(string t){
-		//moves song with title s down one in the playlist.
+		DNode *temp;
+		int count = 0;
+		for (temp=first; temp!=NULL; temp=temp->next){
+			if (temp->song->title==t){
+				if (temp->next==NULL){
+					//cout << "last song"<<endl;
+					string firstTitle = first->song->title;
+					string firstArtist = first->song->artist;
+					int firstMin = first->song->min;
+					int firstSec = first->song->sec;
+					first->song->title = temp->song->title;
+					first->song->artist = temp->song->artist;
+					first->song->min = temp->song->min;
+					first->song->sec = temp->song->sec;
+					temp->song->title = firstTitle;
+					temp->song->artist = firstArtist;
+					temp->song->min = firstMin;
+					temp->song->sec = firstSec;
+				}
+				else if(count==0){
+					//cout << "middle song"<<endl;
+					string nextTitle = temp->next->song->title;
+					string nextArtist = temp->next->song->artist;
+					int nextMin = temp->next->song->min;
+					int nextSec = temp->next->song->sec;
+					temp->next->song->title = temp->song->title;
+					temp->next->song->artist = temp->song->artist;
+					temp->next->song->min = temp->song->min;
+					temp->next->song->sec = temp->song->sec;
+					temp->song->title = nextTitle;
+					temp->song->artist = nextArtist;
+					temp->song->min = nextMin;
+					temp->song->sec = nextSec;
+					count++;
+				}
+			}
+		}
 	}
-									//If it is at the end of the list, it will move to
-									//beginning of the list.
 	void DLL::listDuration(int *tm, int *ts){
 		// gets the total list duration in minutes (passed in as pointers)
 		// and seconds (again, passed in as pointers)
